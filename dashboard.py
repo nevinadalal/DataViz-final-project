2d525c907a42da766416d1ef5e0a2979246bfe4f
nevinadalal <45751214+nevinadalal@users.noreply.github.com>
2022-03-31 01:32:54 +0200

    Added id data.csv

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from page1updated import page1updated
+from page1 import page1
 from page2 import page2
 
 #pictures
@@ -37,10 +37,13 @@ countries = ['Austria',
 def load_full_data():
     df = pd.read_csv("data.csv", index_col = 0)
     return df
-
+@st.cache
+def map_data():
+    df_map = pd.read_csv("data_with_id.csv",index_col = 0)
+    return df_map
 if __name__ == '__main__':
     df = load_full_data()
-
+    df1 = map_data()
     
     page = st.select_slider('Navigate across the pages',['Homepage', 'Page 1', 'Page 2'])
     
@@ -50,7 +53,7 @@ if __name__ == '__main__':
     
     if page =="Page 1":
         st.markdown("# Woman in Decision Making")
-        page1updated(df, countries)
+        page1(df,df1, countries)
 
     if page == "Page 2":
         page2(df, countries)
